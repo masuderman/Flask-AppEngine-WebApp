97dfd52da07171eb4f57c960defd9beeccd1ad8d
Manuel Sudermann <manuelsudermann@gmail.com>
2020-11-13 16:01:41 -0600

    Added placeholder python functions
    In order to test Flask without having to access database, added python functions that modify a preset list rather than the database.

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 #Documentation for query: https://cloud.google.com/appengine/docs/standard/python/datastore/queryclass
 #Documentation for delete: https://www.programcreek.com/python/example/75158/google.appengine.ext.db.delete
 
+############################## Imports + Init Start ####################################
 from google.appengine.ext import db
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+############################## Imports + Init Stop #####################################
 
+############################## App Engine Functions Start ##############################
 #Database Model
 class Person(db.Model): #A simple model to familiarize myself with Datastore and Google App Engine
     name = db.StringProperty(required=True)
@@ -28,11 +31,57 @@ def get_by_name(person_name):
 #stop GET entry
 
 #start DELETE entry
+def delete_by_index(index):
+    person_list = get_list(Person)
+    count = 0
+    for person in person_list:
+        if count == index:
+            Person.delete(person)
+        count += 1
+
 def delete_by_name(person_name):
     person = get_by_name(person_name)
     Person.delete(person)
 #stop DELETE entry
+############################## App Engine Functions Stop ###############################
+
+############################## Python Placeholder Functions Start ######################
+persons = [{'name': 'Jack Bauer'},{'name': 'James Ford'},{'name': 'Charlie Pace'}]
+
+def py_add_person(person_name):
+    persons.append({'name': person_name})
+
+def py_get_list(person_model):
+    return persons
+
+def py_get_by_name(person_name):
+    count = 0
+    for person in persons:
+        if person.name == person_name:
+            return persons[count]
+        count += 1
+
+def py_delete_by_index(index):
+    person_list = persons
+    count = 0
+    for person in person_list:
+        if count == index:
+            person_list.pop(count)
+        count += 1
+
+def py_delete_by_name(person_name):
+    count = 0
+    for person in persons:
+        if person.name == person_name:
+            persons.pop(count)
+        count += 1
+############################## Python Placeholder Functions Stop #######################
 
 #start of Flask code
+
+@app.route("/")
+def home():
+    return "Hello World!"
+
 if __name__ == '__main__':
     app.run(debug=True)
