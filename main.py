c5179fdb2f1095fbf5418ab71231da6f8e539f0e
Manuel Sudermann <manuelsudermann@gmail.com>
2020-11-17 11:23:29 -0600

    Product fully functional
    After removal of templates call in app.yaml with static_dir blocking AppEngine access to home.html and fixing edit entity function, everything works perfectly now.

--- main.py
+++ main.py
@@ -52,12 +52,15 @@ def get_person_by_index(index):
 
 #start EDIT
 def edit_person_name_at_index(index, new_name):
-    all_persons = get_all_persons()
+    all_persons = Person.query()
     count = 0
     for person in all_persons:
         if count == index:
             person.name = new_name
+            person.put()
+            #person.name = new_name
         count += 1
+
 #stop EDIT
 
 #start DELETE
@@ -103,12 +106,5 @@ def handle_delete_or_edit():
         new_person_name = request.form['new_person_name'] #grab user input for new name
         edit_person_name_at_index(button_index, new_person_name) 
     return redirect("/", code=302)
-
-#Handle errors
-@app.errorhandler(500)
-def server_error(e):
-    #Log the error and stacktrace
-    logging.exception('An error occurred during a request. Error: ' + e)
-    return 'An internal error occurred.', 500
 
 ############################## Flask Code Stop #########################################
